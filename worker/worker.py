92ea7ae1cd6030203a516c778479bd7b0e0867c2
sasavlak <sasa.vlaketic@gmail.com>
2026-07-28 11:42:22 +0200

    dodana podatak

--- worker/worker.py
+++ worker/worker.py
@@ -10,12 +10,39 @@ async def health(request):
         }
     )
 
+# Funkcija koja obrađuje primljene podatke
+async def analyze(request):
+    data = await request.json()
+
+    vehicles = data["vehicles"]
+
+    vehicle_count = len(vehicles)
+
+    range_sum = 0
+    maximum_range = 0
+
+    for vehicle in vehicles:
+        electric_range = vehicle["electric_range"]
+
+        range_sum += electric_range
+
+        if electric_range > maximum_range:
+            maximum_range = electric_range
+
+    return web.json_response(
+        {
+            "vehicle_count": vehicle_count,
+            "range_sum": range_sum,
+            "maximum_range": maximum_range
+        }
+    )
 
 # Kreiranje aplikacije
 app = web.Application()
 
 # Registracija HTTP rute
 app.router.add_get("/health", health)
+app.router.add_post("/analyze", analyze)
 
 # Pokretanje servera
 if __name__ == "__main__":
