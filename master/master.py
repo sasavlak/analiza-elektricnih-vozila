b9df3a731b2153864f7d58b9728559a1afa283dd
sasavlak <sasa.vlaketic@gmail.com>
2026-07-28 12:50:11 +0200

    djeljenje podataka

--- master/master.py
+++ master/master.py
@@ -3,17 +3,22 @@ import pandas as pd
 from aiohttp import ClientSession
 
 
+# Adrese Worker servisa
+worker_urls = [
+    "http://127.0.0.1:8081/analyze",
+    "http://127.0.0.1:8082/analyze"
+]
+
+
 # Učitavanje CSV datoteke
 data = pd.read_csv("podaci/elektricna_vozila.csv")
 
 
-async def send_to_worker():
-
+# Funkcija pretvara jedan dio DataFramea u listu rječnika
+def prepare_vehicles(data_chunk):
     vehicles = []
 
-    # Pretvaranje DataFramea u listu rječnika
-    for _, row in data.iterrows():
-
+    for _, row in data_chunk.iterrows():
         vehicles.append(
             {
                 "manufacturer": row["manufacturer"],
@@ -21,18 +26,48 @@ async def send_to_worker():
             }
         )
 
+    return vehicles
+
+
+# Funkcija šalje jedan dio podataka jednom Workeru
+async def send_to_worker(session, worker_url, vehicles):
+    async with session.post(
+        worker_url,
+        json={"vehicles": vehicles}
+    ) as response:
+
+        result = await response.json()
+
+        return result
+
+
+async def main():
+
+    # Podjela podataka na dva približno jednaka dijela
+    data_chunks = [
+        data.iloc[::2],
+        data.iloc[1::2]
+    ]
+
     async with ClientSession() as session:
 
-        async with session.post(
-            "http://127.0.0.1:8081/analyze",
-            json={"vehicles": vehicles}
-        ) as response:
+        results = []
 
-            result = await response.json()
+        for index in range(len(worker_urls)):
+
+            vehicles = prepare_vehicles(data_chunks[index])
+
+            result = await send_to_worker(
+                session,
+                worker_urls[index],
+                vehicles
+            )
+
+            results.append(result)
 
             print()
-            print("Rezultat koji je vratio Worker:")
+            print(f"Rezultat Workera {index + 1}:")
             print(result)
 
 
-asyncio.run(send_to_worker())
+asyncio.run(main())
